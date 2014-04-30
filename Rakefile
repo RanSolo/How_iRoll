f7f3bd9baec29ce8a13abfc9db232918f25f20ea
Randy Russell <ransolo@me.com>
2014-04-30 00:31:15 -0500

    working on adding rspec

--- Rakefile
+++ Rakefile
@@ -1,10 +1,17 @@
 #!/usr/bin/env ruby
 # -*- ruby -*-
 
-require 'rake/testtask'
-Rake::TestTask.new() do |t|
-  t.pattern = "test/test_*.rb"
-end
+require 'rspec/core/rake_task'
 
-desc "Run tests"
-task :default => :test
+RSpec::Core::RakeTask.new(:spec)
+
+task :default => :spec
+
+
+# require 'rake/testtask'
+# Rake::TestTask.new() do |t|
+#   t.pattern = "test/test_*.rb"
+# end
+#
+# desc "Run tests"
+# task :default => :test
--- test/test_How_iRoll.rb
+++ test/test_How_iRoll.rb
@@ -1,29 +1,29 @@
-require_relative 'helper'
-require 'test/unit'
-
-class TestMenuIntegration < MiniTest::Unit::TestCase
-
-  def test_menu_prints_out
-     shell_output = ""
-     IO.popen('lib/how_iRoll.rb', 'r+') do |pipe|
-       inputs.each do |input|
-      # pipe.puts input
-      pipe.close_write
-      shell_output = pipe.read
-    end
-    expected_output = <<EOS
-How_iRoll is a fun little app to help log your journies... on wheels.
-If you're walking or flying you can walk or fly on to the next app buddy.
-At any type "opt" for available options or enter new options by typing
-'enter'...  i.e. available options for our first question are: 'bike' or
-'bicycle'; 'sd' or 'Sunday driver'; skate or 'skateboard'; 'moto' or
-'motorcycle'; 'hid' or 'hidden options'.
-
-Ask yourself this first question and use the options above to answer...
-
-How_iRoll?
-EOS
-    assert_equal expected_output, shell_output
-  end
-  end
-end
+# require_relative 'helper'
+# require 'test/unit'
+#
+# class TestMenuIntegration < MiniTest::Unit::TestCase
+#
+#   def test_menu_prints_out
+#      shell_output = ""
+#      IO.popen('lib/how_iRoll.rb', 'r+') do |pipe|
+#        inputs.each do |input|
+#       # pipe.puts input
+#       pipe.close_write
+#       shell_output = pipe.read
+#     end
+#     expected_output = <<EOS
+# How_iRoll is a fun little app to help log your journies... on wheels.
+# If you're walking or flying you can walk or fly on to the next app buddy.
+# At any type "opt" for available options or enter new options by typing
+# 'enter'...  i.e. available options for our first question are: 'bike' or
+# 'bicycle'; 'sd' or 'Sunday driver'; skate or 'skateboard'; 'moto' or
+# 'motorcycle'; 'hid' or 'hidden options'.
+#
+# Ask yourself this first question and use the options above to answer...
+#
+# How_iRoll?
+# EOS
+#     assert_equal expected_output, shell_output
+#   end
+#   end
+# end
